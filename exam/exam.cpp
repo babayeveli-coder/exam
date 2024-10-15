d9eac655840514f8c6f16bd4120d33f7daa25b86
babayeveli-coder <eb39200@gmail.com>
2024-10-15 17:30:11 +0400

    variable was prepared

--- exam/exam.cpp
+++ exam/exam.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
-}
+#include <string>
+#include <conio.h>
 
+using namespace std;
+
+const int MAX_QUESTIONS = 100;
+const int MAX_OPTIONS = 4;
+string quizTitles[MAX_QUESTIONS];
+string quizQuestions[MAX_QUESTIONS][MAX_QUESTIONS];
+string quizOptions[MAX_QUESTIONS][MAX_QUESTIONS][MAX_OPTIONS];
+int quizAnswers[MAX_QUESTIONS][MAX_QUESTIONS];
+int numQuizzes = 0;
+int numQuestions[MAX_QUESTIONS] = { 0 };
